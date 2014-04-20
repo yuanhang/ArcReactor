e497c781eaa57311ab95af5af4b180ad27154c96
yuanhang <yuanhang.guo@gmail.com>
2014-04-20 18:07:08 +0800

    implement check_uniq, add more specs

--- lib/crack/1.1.rb
+++ lib/crack/1.1.rb
@@ -1,5 +1,14 @@
 class UniqueChecker
   def self.check_uniq s
+    return false if s.nil?
+    hash = {}
+    s.each_char do |c|
+      if hash.has_key? c
+        return false
+      else
+        hash[c] = true
+      end
+    end 
     return true
   end
 end
--- spec/crack/1.1_spec.rb
+++ spec/crack/1.1_spec.rb
@@ -2,12 +2,26 @@ require 'crack'
 
 describe UniqueChecker do
   describe "#check_uniq" do
-    context "unique character string" do
-      it "returns true when characters are unique" do
-        str = "abcde"
-        actual = UniqueChecker.check_uniq str
-        expect(actual).to eq(true)
-      end
+
+    def assert_uniq expect, s
+      actual = UniqueChecker.check_uniq s
+      expect(actual).to eq(expect)
+    end
+
+    it "returns true when characters are unique" do
+      assert_uniq true, "abcde"
+    end
+
+    it "returns false when the characters are not unique" do 
+      assert_uniq false, "aabb"
+    end
+
+    it "returns true when string is empty" do
+      assert_uniq true, ""
+    end
+
+    it "returns false when string is nil" do
+      assert_uniq false, nil 
     end
   end
 end
