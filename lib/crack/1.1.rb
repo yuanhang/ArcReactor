a460a10d0038fc1175f76a0896c0e6221c2b8051
yuanhang <yuanhang.guo@gmail.com>
2014-04-20 18:18:48 +0800

    add specs to cover non-aphabet characters

--- lib/crack/1.1.rb
+++ lib/crack/1.1.rb
@@ -1,9 +1,11 @@
 class UniqueChecker
   def self.check_uniq s
     return false if s.nil?
+    return false if !s.is_a?(String)
     hash = {}
     s.each_char do |c|
       if hash.has_key? c
+        puts "Found duplicate key, #{c}"
         return false
       else
         hash[c] = true
--- spec/crack/1.1_spec.rb
+++ spec/crack/1.1_spec.rb
@@ -24,8 +24,32 @@ describe UniqueChecker do
       assert_uniq false, nil 
     end
 
-    it "returns false when input is a number" do
-      assert_uniq false, 5 
+    context "when input is not a stirng" do
+      it "returns false when input is a fixnum" do
+        assert_uniq false, 5 
+      end
+
+      it "returns false when input is a float" do
+        assert_uniq false, 5.0
+      end
+
+      it "returns false when input is a array" do
+        assert_uniq false, [] 
+      end
+
+      it "returns false when input is a hash" do
+        assert_uniq false, {} 
+      end
+    end
+
+    context "when input string contains non alphabet char" do
+      it "returns false when input has same non-alphabet char" do
+        assert_uniq false, "^abc^%" 
+      end
+
+      it "returns true when input has no same char" do
+        assert_uniq true, 'abc!@#$%^&*()_+{}|:"<>?1234567890-=[]\;,./' 
+      end
     end
   end
 end
